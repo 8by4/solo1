bf24003d1fb14b1f2dd896af385fb33ae42a6dd7
8by4 <aidanquinn147@gmail.com>
2024-02-01 13:26:03 -0500

    evan has big diuck

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -10,7 +10,6 @@ const WALL_JUMP_POWER = 500
 const WALLJUMP_TIME = 0.2
 
 # Movement Variables
-var motion = Vector2(0, 0)
 var justWallJumped = false
 
 # Node References
@@ -29,28 +28,28 @@ func _process(delta):
 	# (Wall) Jumping
 	if Input.is_action_just_pressed("up"):
 		if is_on_floor():
-			motion.y = -JUMP_POWER
+			velocity.y = -JUMP_POWER
 		else:
 			if rayCastLeftNode.is_colliding() or rayCastRightNode.is_colliding():
-				motion.y = -JUMP_POWER * 0.8
+				velocity.y = -JUMP_POWER * 0.8
 				justWallJumped = true
 				timerNode.start(WALLJUMP_TIME)
 			
 			if rayCastLeftNode.is_colliding():
-				motion.x = WALL_JUMP_POWER
+				velocity.x = WALL_JUMP_POWER
 			if rayCastRightNode.is_colliding():
-				motion.x = -WALL_JUMP_POWER
+				velocity.x = -WALL_JUMP_POWER
 				
 	if not justWallJumped:
 		if Input.is_action_pressed("left"):
-			motion.x -= SPEED
+			velocity.x -= SPEED
 		elif Input.is_action_pressed("right"):
-			motion.x += SPEED
+			velocity.x += SPEED
 			
-		motion.x = lerpf(motion.x, 0.0, 0.3)
+		velocity.x = lerpf(velocity.x, 0.0, 0.3)
 	
-	motion.y += GRAVITY
+	velocity.y += GRAVITY
 	
-	if MAX_DOWN_VEL < motion.y:
-		motion.y = MAX_DOWN_VEL
+	if MAX_DOWN_VEL < velocity.y:
+		velocity.y = MAX_DOWN_VEL
 	move_and_slide()
