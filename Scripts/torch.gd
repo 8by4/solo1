b48c241bfbdd494ead431f1bd28c5e19e51cb4b3
8by4 <aidanquinn147@gmail.com>
2024-02-05 18:44:25 -0500

    torch anim

--- Scripts/torch.gd
+++ Scripts/torch.gd
@@ -6,7 +6,8 @@ func _input(event):
 	# torch activation
 	if active == true:
 		if Input.is_action_just_pressed("interaction"):
-			$Sprite2D.frame = 1
+			$AnimationPlayer.play("ignite")
+			$AnimationPlayer.play("")
 			$Lighting.texture_scale = 3
 			$Smoke.visible = true
 func _on_interaction_area_body_entered(body):
